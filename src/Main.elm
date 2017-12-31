7485aeb733977762ffb802cbc2058a7424a1c94d
Robert Vollmert <rob@vllmrt.net>
2017-12-31 17:25:46 +0100

    translation helper

--- src/Main.elm
+++ src/Main.elm
@@ -41,21 +41,33 @@ view cards =
         d t c =
             Svg.g [ Svg.transform t ] [ SvgSet.draw c ]
 
+        trans ( r, c ) =
+            let
+                x =
+                    30 + 60 * c
+
+                y =
+                    50 + 90 * r
+            in
+            "translate(" ++ toString x ++ "," ++ toString y ++ ")"
+
         gs =
             zipWith d
-                [ "translate(30,50)"
-                , "translate(90,50)"
-                , "translate(150,50)"
-                , "translate(210,50)"
-                , "translate(30,140)"
-                , "translate(90,140)"
-                , "translate(150,140)"
-                , "translate(210,140)"
-                , "translate(30,230)"
-                , "translate(90,230)"
-                , "translate(150,230)"
-                , "translate(210,230)"
-                ]
+                (List.map trans
+                    [ ( 0, 0 )
+                    , ( 0, 1 )
+                    , ( 0, 2 )
+                    , ( 0, 3 )
+                    , ( 1, 0 )
+                    , ( 1, 1 )
+                    , ( 1, 2 )
+                    , ( 1, 3 )
+                    , ( 2, 0 )
+                    , ( 2, 1 )
+                    , ( 2, 2 )
+                    , ( 2, 3 )
+                    ]
+                )
                 cards
     in
     Svg.svg [ Svg.viewBox "0 0 300 300", Svg.width "500px" ]
