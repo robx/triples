8553e5ab3319a0ce406515935052b00b36ebae68
Robert Vollmert <rob@vllmrt.net>
2018-01-26 23:29:11 +0100

    Revert "always allow deal more (experimetn)"
    This reverts commit 554540962be5537537dd28fb435a9f84ae2bd410.

--- client/src/Play.elm
+++ client/src/Play.elm
@@ -353,5 +353,7 @@ update now msg model =
             in
             if over then
                 ( model, Just <| GameOver <| ( now, EEnd ) :: model.log )
-            else
+            else if nsets == 0 then
                 ( { model | game = Game.dealMore model.game, answer = Nothing, log = ( now, EDealMoreZero ) :: model.log }, Nothing )
+            else
+                ( { model | answer = Just (Game.count model.game), log = ( now, EDealMoreNonzero ) :: model.log }, Nothing )
